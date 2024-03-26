69afb5728ea189350bae594bf7a8db3d35cbff5c
Toy Santana António <toysan3@hotmail.com>
2024-03-26 18:00:38 +0100

    Update

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -7,6 +7,7 @@ import {
   setBalance,
   googleLogIn,
 } from "./operations";
+import { history } from "../utils/history";
 
 const initialState = {
   user: { email: null, balance: null, avatarUrl: null },
@@ -18,51 +19,64 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
+
   reducers: {
     login: (state, action) => {
       state.token = action.payload;
       state.isLoggedIn = true;
-      localStorage.setItem("isLoggedIn", true); // Add this line
+      localStorage.setItem("isLoggedIn", true);
     },
+
     logout: (state) => {
       state.token = null;
       state.isLoggedIn = false;
-      localStorage.removeItem("isLoggedIn"); // Add this line
+      localStorage.removeItem("isLoggedIn");
     },
   },
+
   extraReducers: (builder) => {
     builder
+
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.user.token;
         state.isLoggedIn = true;
       })
+
       .addCase(logIn.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.user.token;
         state.isLoggedIn = true;
+
+        history.push("/main");
       })
+
       .addCase(googleLogIn.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.user.token;
         state.isLoggedIn = true;
       })
+
       .addCase(logOut.fulfilled, (state) => {
         state.user = { email: null };
         state.token = null;
         state.isLoggedIn = false;
       })
+
       .addCase(refreshUser.pending, (state) => {
         state.isRefreshing = true;
       })
+
       .addCase(refreshUser.fulfilled, (state, action) => {
         state.user = action.payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
       })
+
       .addCase(refreshUser.rejected, (state) => {
         state.isRefreshing = false;
       })
+
       .addCase(setBalance.fulfilled, (state, action) => {
         state.user.balance = action.payload.balance;
         state.isRefreshing = false;
